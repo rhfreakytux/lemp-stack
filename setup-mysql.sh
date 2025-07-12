e7395ba7d9a712ac084b598d1cd0f46609da3d2f
rhfreakytux <narayanix@proton.me>
2025-07-12 08:52:30 +0545

    added database initialization

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export DB_PASSWORD=P@ssw0rd@123
+
 # Get the latest module stream number
 get_latest_version(){
 latest_mysql_stream=mysql:$(dnf module list mysql \
@@ -13,7 +15,7 @@ latest_mysql_stream=mysql:$(dnf module list mysql \
 install_mysql(){
   dnf module disable mysql -y 
   dnf module enable "$latest_mysql_stream" -y
-  dnf install @mysql -y 
+  dnf install @mysql expect -y 
 }
 
 #Manage Service
@@ -36,7 +38,46 @@ update_firewall(){
   fi
 }
 
+#Initialize Databse
+mysql_installation() {
+
+  expect <<EOF
+
+  spawn mysql_secure_installation
+
+  expect "Would you like to setup VALIDATE PASSWORD component?"
+  send "y\r"
+
+  expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
+  send "2\r"
+
+  expect "Please set the password for root here:"
+  send "$DB_PASSWORD\r"
+
+  expect "Re-enter new password:"
+  send "$DB_PASSWORD\r"
+
+  expect "Do you wish to continue with the password provided?"
+  send "y\r"
+
+  expect "Remove anonymous users?"
+  send "y\r"
+
+  expect "Disallow root login remotely?"
+  send "y\r"
+
+  expect "Remove test database and access to it?"
+  send "y\r"
+
+  expect "Reload privilege tables now?"
+  send "y\r"
+
+  expect eof
+EOF
+}
+
 get_latest_version
 install_mysql
 service_enable
 update_firewall
+mysql_installation
